6b6ff669ebd12cf7497d324653e98a9270de10e5
TzurV <tzurvaich@gmail.com>
2020-06-18 17:04:54 +0100

    load into a single dataframer

--- Analyze.py
+++ Analyze.py
@@ -7,10 +7,17 @@ if __name__ == "__main__":
     allFundsInf = pd.DataFrame()
     for file in glob.glob("*_FundsInf.csv"):
         print(file)
-        #fundInf = pd.read_csv(file, sep=',')
-        #allFundsInf = allFundsInf.append(fundInf, ignore_index=True)
-        #print(fundInf.head())
-        #print(fundInf.shape)
+        fundInf = pd.read_csv(file, sep=',')
+        allFundsInf = allFundsInf.append(fundInf, ignore_index=True)
+        print(fundInf.head())
+        print(fundInf.shape)
+        print(fundInf.columns)
+
+print("-------- ALL ----------------")
+print(allFundsInf.shape)
+print(allFundsInf.head())
+print(allFundsInf.columns)
+print(allFundsInf.head())
 
 
-
+print(allFundsInf.iloc[:, 0:5])
