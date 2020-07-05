f7c87112a1c5f167e1fd1e37eda9741efe001cc1
TzurV <tzurvaich@gmail.com>
2020-07-05 19:07:40 +0100

    checks risk and quartile

--- Analyze.py
+++ Analyze.py
@@ -4,6 +4,7 @@ import glob, os
 
 if __name__ == "__main__":
 
+    print("#=====================================================")
     # create empty dataframe
     allFundsInf = pd.DataFrame()
     for file in glob.glob("*_FundsInf.csv"):
@@ -21,24 +22,30 @@ if __name__ == "__main__":
 
     print("-------- ALL ----------------")
     print(allFundsInf.shape)
+    print(allFundsInf.columns)
     #print(allFundsInf.head())
     #print(allFundsInf.columns)
-    print(allFundsInf.head())
+    #print(allFundsInf.head())
 
+    #=============================
     #                      3 first rows and 5 columns
-    print(allFundsInf.iloc[:3, 0:5])
+    #print(allFundsInf.iloc[:3, 0:5])
     #   Unnamed: 0            date                                       fundName  Quartile  FERisk
     #0           0  14/06/20 23:33  HSBC World Selection Cautious Portfolio C Acc       NaN    27.0
     #1           1  14/06/20 23:33                         Jupiter European I Acc       2.0    79.0
     #2           2  14/06/20 23:33                   Liontrust Global Alpha C Acc       1.0    89.0
 
+    #==========================
     # Points (like histogram)
-    print(allFundsInf.groupby('fundName').fundName.count())
-    fundsList = allFundsInf.groupby('fundName')
-    print(type(fundsList))
+    #print(allFundsInf.groupby('fundName').fundName.count())
+    #fundsList = allFundsInf.groupby('fundName')
+    #print(type(fundsList))
 
+    # ===========================
+    # https://www.shanelynn.ie/summarising-aggregation-and-grouping-data-in-python-pandas/
     print(allFundsInf.groupby('Sector').Sector.count())
-    AllSectors = allFundsInf.groupby('Sector')
+    print(allFundsInf.groupby(['fundName', 'date'])['date'].count())
+    #AllSectors = allFundsInf.groupby('Sector')
 
     #https://realpython.com/pandas-groupby/
     #groupedFundsList = allFundsInf.groupby(['Sector','fundName', 'date'], as_index=False)
@@ -46,16 +53,37 @@ if __name__ == "__main__":
     #for (sector, fund, date, frame) in groupedFundsList:
     #    print(sector, fund, date)
 
+    # create report
+    COLUMN_NAMES=['fundName', 'worsenQuartile', 'worsenFERisk']
+    pdSummary = pd.DataFrame(columns=COLUMN_NAMES)
+
+    # group by fundname
     groupedFundsList = allFundsInf.groupby('fundName', as_index=False)
-    print(groupedFundsList)
+    #print(groupedFundsList)
     for fund, frame in groupedFundsList:
         print(fund)
-        print(frame.sort_values(by="date"))
+        #print(frame.sort_values(by="date"))
         sortedFunds = frame.sort_values(by="date")
-        for i in range(len(sortedFunds.index)):
-            print(sortedFunds.FERisk.iloc[i])
+        print(sortedFunds[['date', 'Quartile', 'FERisk', '3m', '6m', '1y', '3y', '5y' ]])
+        
+        # check if Quartile and FERisk is different
+        lastQuartile = sortedFunds.Quartile.iloc[0]
+        worsenQuartile = False
+        lastFERisk = sortedFunds.FERisk.iloc[0]
+        worsenFERisk = False
+        for i in range(1, len(sortedFunds.index)):
+            if(lastQuartile>sortedFunds.Quartile.iloc[i]):
+                worsenQuartile = True
+            if(lastFERisk*0.95>sortedFunds.FERisk.iloc[i]):
+                worsenFERisk = True
+        if worsenQuartile or worsenFERisk:
+            print("\t#==================== Check this one =================")
+            print(f"\t# {worsenQuartile}: worsen Quartile, higher FERisk: {worsenFERisk}  \n")
+            pdSummary = pdSummary.append({'fundName':fund, 'worsenQuartile':worsenQuartile, 'worsenFERisk':worsenFERisk}, ignore_index=True)
+            
 
-
+    print("====== Observations summary ============")
+    print(pdSummary)
 
     #print(type(A), A.shape)
     #AllSectorsdict = AllSectors.to_dict()
