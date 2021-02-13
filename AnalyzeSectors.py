9732b62f1e7adc17074f998aa253ff71dbc9ec90
TzurV <tzurvaich@gmail.com>
2021-02-13 12:11:13 +0000

    Update AnalyzeSectors.py

--- AnalyzeSectors.py
+++ AnalyzeSectors.py
@@ -1,10 +1,25 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import glob
 from datetime import datetime
 import json
+import sys
+# https://matplotlib.org/stable/gallery/misc/multipage_pdf.html
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+# https://matplotlib.org/2.0.2/examples/lines_bars_and_markers/marker_reference.html
+from six import iteritems
+from matplotlib.lines import Line2D
 
 import warnings
 warnings.filterwarnings('ignore')
+
+now = datetime.now()
+
+current_time = now.strftime("%d/%m/%y %H:%M")
+print("Current Time =", current_time)
+dateStamp = now.strftime("%Y%m%d")
 
 
 def addPerformanceOrder(sectorsInf, columnName):
@@ -18,7 +33,7 @@ def addPerformanceOrder(sectorsInf, columnName):
     #    print(sector)
     return sortedByColumn
 
-def loadSectorInf():
+def loadSectorInf(historyWeeks = 8):
     print("# Load Sector Information =====================================================")
     # create empty dataframe
     sectorSelectedColumns = ['1m', '3m', '6m', '1y', '3y', '5y']
@@ -27,7 +42,7 @@ def loadSectorInf():
     file = None
     sectorTop5counter = {}
     allFilesList = glob.glob("*_TrustNetSectors.csv")
-    for file in allFilesList[-12:]:
+    for file in allFilesList[-historyWeeks:]:
         #print(f"Loading {file}" )
         totalSectorsInfFiles += 1
 
@@ -67,13 +82,135 @@ def loadSectorInf():
     return allSectorsInf, sectorTop5counter
     
 
+def getPlotInformation(allSectorsInf, sectorTop5counter):
+    ''' 
+    collect passed information on top 5 
+    '''
 
+    # Filter out filled markers and marker settings that do nothing.
+    # We use iteritems from six to make sure that we get an iterator
+    # in both python 2 and 3
+    unfilled_markers = [m for m, func in iteritems(Line2D.markers)
+                        if func != 'nothing' and m not in Line2D.filled_markers]
+
+
+    # leave just the ones that are at least half than the best count
+    # the list is sorted
+    for indx, name in enumerate(list(sectorTop5counter)):
+        if indx==0:
+            bestCount = int(sectorTop5counter[name]/2)
+            continue
+        if sectorTop5counter[name]<bestCount:
+            del sectorTop5counter[name]
+        
+    # group by fundname (code)
+    groupedFundsList = allSectorsInf.groupby('sectorName', as_index=False)
+
+    
+
+    pdfFileName = dateStamp+'_SectorsPerformance_all.pdf'
+    with PdfPages(pdfFileName) as pdf:        
+        ax  = plt.gca()
+        markerIndx = 0
+        for sectorName, frame in groupedFundsList:
+        
+            if not sectorName in sectorTop5counter:
+                continue
+            
+            # set marker
+            markerIndx += 1
+            markerIndx %= len(unfilled_markers)
+    
+            print(f"# processing {sectorName}")
+            sortedFunds = frame.drop_duplicates(subset ="date", keep = False)
+            
+            # date  column becomes index column
+            sortedFunds.set_index('date', drop=False, append=False, inplace=True, verify_integrity=False)
+            sortedFunds.sort_index()
+            #print(sortedFunds.head())
+            
+            # sector order
+            colName = sectorName + "-O_1m"
+            colName = colName.replace(' ','_')
+            sortedFunds.rename(columns={"O_1m":colName}, inplace=True)
+    
+            sortedFunds.plot(kind='line', x='date',
+                             marker=unfilled_markers[markerIndx],
+                             y=colName, ax=ax)        
+                
+        
+        # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.legend.html
+        ax.legend(loc='best', fontsize='x-small', ncol=2)
+        plt.grid(True)
+        # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.savefig.html
+    
+        # pdfFileName = dateStamp+'_SectorsPerformance_all.pdf'
+        # plt.savefig(pdfFileName)  
+        # plt.savefig(dateStamp+'_SectorsPosition.pdf')  
+        pdf.savefig()  # saves the current figure into a pdf page
+    
+        # https://stackoverflow.com/questions/51203839/python-saving-empty-plot/51203929
+        plt.show()
+        plt.close()
+    
+    
+        ax  = plt.gca()
+        markerIndx = 0
+        for sectorName, frame in groupedFundsList:
+        
+            if not sectorName in sectorTop5counter:
+                continue
+            
+            # set marker
+            markerIndx += 1
+            markerIndx %= len(unfilled_markers)
+    
+            print(f"# processing {sectorName}")
+            sortedFunds = frame.drop_duplicates(subset ="date", keep = False)
+            
+            # date  column becomes index column
+            sortedFunds.set_index('date', drop=False, append=False, inplace=True, verify_integrity=False)
+            sortedFunds.sort_index()
+            #print(sortedFunds.head())
+            
+        
+            # sector last month performance 
+            colName = sectorName + "-1m"
+            colName = colName.replace(' ','_')
+            sortedFunds.rename(columns={"1m":colName}, inplace=True)
+    
+            sortedFunds.plot(kind='line', x='date',
+                             marker=unfilled_markers[markerIndx],
+                             y=colName, ax=ax)        
+    
+    
+        ax.legend(loc='best', fontsize='x-small', ncol=2)
+        plt.grid(True)
+        # plt.savefig(dateStamp+'_SectorsPerformance.pdf')  
+        # plt.savefig(pdfFileName)  
+        pdf.savefig()  # saves the current figure into a pdf page
+        plt.show()
+        plt.close()
+
+        # We can also set the file's metadata via the PdfPages object:
+        d = pdf.infodict()
+        d['Title'] = 'Multipage PDF Example'
+        d['Author'] = 'Tzur Vaich'
+        d['CreationDate'] = datetime.now()
+   
 
 '''
 '''
 
 if __name__ == "__main__":
 
-    allSectorsInf, sectorTop5counter = loadSectorInf()
+    allSectorsInf, sectorTop5counter = loadSectorInf(historyWeeks = 8)
     print(json.dumps(sectorTop5counter, indent=2))
+    
+    if not len(allSectorsInf):
+        print("# Check working directory, no data was found. Aborting.")
+        sys.exit(0)
+        
+        
+    getPlotInformation(allSectorsInf, sectorTop5counter)
     
