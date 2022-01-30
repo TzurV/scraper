36c11f40b91e1bc60af4fd94e850e582561a8996
TzurV <tzurvaich@gmail.com>
2022-01-30 11:33:29 +0000

    Update MyScraper.py

--- MyScraper.py
+++ MyScraper.py
@@ -61,7 +61,7 @@ from openpyxl import load_workbook
 class MyHoldingsExcell:
     def __init__(self):
         
-        self.AllHoldingsFile = r"E:\Family\צור\bank and money\Fidelity\AllHoldings_Updated.xlsx"
+        self.AllHoldingsFile = r"E:\Family\צור\bank and money\Investments_reports\AllHoldings_Updated.xlsx"
         print(f"# getting iformation from {self.AllHoldingsFile}.")
         self.workbook = load_workbook(filename=self.AllHoldingsFile, data_only=True)
         pass
